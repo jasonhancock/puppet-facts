bbf1e943ed95e8ad9e34ee43d8f91b67f9d4d5f6
Jason Hancock <jsnbyh@gmail.com>
2012-08-21 07:39:05 -0700

    remote whitespace

--- runlevel.rb
+++ runlevel.rb
@@ -23,9 +23,10 @@
 require 'facter'
 
 ENV['PATH']='/bin:/sbin:/usr/bin:/usr/sbin'
- 
+
 result = %x{runlevel | awk '{print $2}'}
- 
+result.strip!
+
 Facter.add('runlevel') do
     setcode do
         result
